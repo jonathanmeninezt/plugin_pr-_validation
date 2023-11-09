ca397298c10ce76f69496a2db9254f972482891b
Jonathan Meninez <jonathan.meninezt@gmail.com>
2023-11-09 16:37:49 -0300

    criação conexão einserido informações Create/FetchXML

--- MeuPrimeiroProjeto/Conexao.cs
+++ MeuPrimeiroProjeto/Conexao.cs
@@ -6,20 +6,19 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MeuPrimeiroProjeto
-{
-    internal class Conexao
-    {
+namespace MeuProjetoInicial {
+    internal class Conexao {
         private static CrmServiceClient crmServiceClientTreinamento; // variável do tipo CrmServiceCliente
         public CrmServiceClient ObterConexao() // método que devolve uma CrmServiceCliente
         {
             // variável contendo a ConnectionString
             var connectionStringCRM = @"AuthType=OAuth;
-             Username = userdyn365@treindio.onmicrosoft.com;
-             Password = Pass@word99; SkipDiscovery = True;
-             AppId = 51f81489-12ee-4a9e-aaae-a2591f45987d;
-             RedirectUri = app://58145B91-0C36-4500-8554-080854F2AC97;
-             Url = https://org07de2ee3.crm2.dynamics.com/main.aspx;";
+                                     Username = 202304067317@alunos.estacio.br;
+                                     Password = Tec767821@; SkipDiscovery = True;
+                                     AppId = 51f81489-12ee-4a9e-aaae-a2591f45987d;
+                                     RedirectUri = app://58145B91-0C36-4500-8554-080854F2AC97;
+                                     Url = https://org14dcc1da.crm2.dynamics.com/main.aspx;";
+
             if (crmServiceClientTreinamento == null) // verifica se a variável crmServiceClientTrinamento está vazia
             {
                 System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12; // define o protocolo segurança
--- MeuPrimeiroProjeto/Program.cs
+++ MeuPrimeiroProjeto/Program.cs
@@ -1,16 +1,66 @@
-﻿using System;
+﻿using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MeuPrimeiroProjeto
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            var crmService = new Conexao().ObterConexao();   
+namespace MeuProjetoInicial {
+    internal class Program {
+        static void Main(string[] args) {
+            var crmService = new Conexao().ObterConexao();
+            //FetchXML(crmService);
         }
+        static void FetchXML(CrmServiceClient crmService) // recebe como parâmetro a conexão - Método
+        {
+            string query = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
+                             <entity name='account'>
+                             <attribute name='name' />
+                             <attribute name='telephone1' />
+                             <attribute name='accountid' />
+                             <attribute name='emailaddress1' />
+                             <order attribute='name' descending='false' />
+                             </entity>
+                             </fetch>"; // declara variável do tipo String contendo o FechXML da consulta
+
+            EntityCollection colecao = crmService.RetrieveMultiple(new FetchExpression(query)); // declara variável do tipo
+            foreach (var item in colecao.Entities) // para cada entidade (registro) armazenará na variável item
+            {
+                Console.WriteLine(item["name"]); // exibe o atributo name
+                if (item.Attributes.Contains("telephone1")) // verifica se retornou o atributo telephone1
+                {
+                    Console.WriteLine(item["telephone1"]); // exibe o atributo telephone
+                }
+            }
+            Console.ReadKey(); // aguarda o usuário pressionar qualquer tecla para continuar
+        }
+
+
+
+        static void Create(CrmServiceClient crmService) // recebe como parâmetro a conexão
+
+
+{
+            Guid newRecord = new Guid(); // declara variável do tipo Guid
+            Entity newEntity = new Entity("account"); // declara variável do Entity a partir da entidade Account
+            newEntity.Attributes.Add("name", "Conta Criada em Treinamento - " + DateTime.Now.ToString()); // nome
+            newEntity.Attributes.Add("telephone1", "11985326471"); // telefone
+            newEntity.Attributes.Add("emailaddress1", "contato@provedor.com"); // e-mail
+            newRecord = crmService.Create(newEntity); // executa o método Create e armazena na variável newRecord o Guid retornado do novo registro no Dataverse. 
+
+            if (newRecord != Guid.Empty) // verificar se a variável newRecord contém um Guid
+{
+                Console.WriteLine("Id do Registro criado : " + newRecord); // exibe informação no console do usuário
+            } else // caso contrário
+              {
+                Console.WriteLine("newRecord não criado!"); // exibe informação que não foi criado o registro
+            }
+            Console.ReadKey(); // aguarda o usuário pressionar qualquer tecla para continuar
+        }
+
+
     }
 }
